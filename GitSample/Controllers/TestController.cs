765f5691d7900798d7ee4dfeaf24266c598788f1
HARIHANTH M <harihanthmanohar@gmail.com>
2026-02-05 16:42:59 +0530

    Added Test Controller

---
+++ GitSample/Controllers/TestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GitSample_CodeBeings.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
